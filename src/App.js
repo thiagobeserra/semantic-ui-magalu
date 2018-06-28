ad9f9fbca7395e15d6b63920c891a186a0b939b9
thiago beserra <thiago.beserra@luizalabs.com>
2018-06-28 10:34:43 -0300

    Criação de tema padrão adminsitrativo Magalu utilizando a biblioteca Semantic-UI

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './semantic/dist/semantic.min.css';
+
+import { Button, Menu, Dropdown } from 'semantic-ui-react';
+import { Router, Link } from 'react-router-dom';
+
+class App extends Component {
+
+  state = {
+    activeItem: 'home'
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+
+    const { activeItem } = this.state
+
+    return (
+      <div className="App">
+
+        <div>
+          <Menu secondary simple pointing>
+
+            <Menu.Item
+              name='messages'
+              active={activeItem === 'messages'}
+              onClick={this.handleItemClick} />
+
+            <Menu.Item
+              name='friends'
+              active={activeItem === 'friends'}
+              onClick={this.handleItemClick} />
+
+            <Dropdown simple text='Shopping' name='shopping' className="link item" active={activeItem === 'shopping'} onClick={this.handleItemClick}>
+              <Dropdown.Menu>
+                <Dropdown.Header>Categories</Dropdown.Header>
+                <Dropdown.Item>Teste</Dropdown.Item>
+                <Dropdown.Item>
+                  <Dropdown simple text='Clothing'>
+                    <Dropdown.Menu>
+                      <Dropdown.Header>Mens</Dropdown.Header>
+                      <Dropdown.Item>Shirts</Dropdown.Item>
+                      <Dropdown.Item>Pants</Dropdown.Item>
+                      <Dropdown.Item>Jeans</Dropdown.Item>
+                      <Dropdown.Item>Shoes</Dropdown.Item>
+                      <Dropdown.Divider />
+                      <Dropdown.Header>Womens</Dropdown.Header>
+                      <Dropdown.Item>Dresses</Dropdown.Item>
+                      <Dropdown.Item>Shoes</Dropdown.Item>
+                      <Dropdown.Item>Bags</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+          </Menu>
+
+        </div>
+
+        <Button primary>Botão Primário Semantic UI Magalu</Button>
+
+        <Button positive>Botão Positive Semantic UI Magalu</Button>
+
+        <Button negative>Botão Negative Semantic UI Magalu</Button>
+
+        <Button secondary>Botão Secundario Semantic UI Magalu</Button>
+
+        <Button disabled>Botão Desabilitado Semantic UI Magalu</Button>
+
+        <a href="#">Link Semantic UI Magalu</a>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
